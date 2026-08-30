46e5bc5d0ccb50a22ed6dea076868fce564bd57b
blaysmp67-rgb <blaysmp67@gmail.com>
2026-08-30 15:40:15 -0400

    Delete daymark_permissions.sql

--- daymark_permissions.sql
+++
@@ -1,4 +0,0 @@
-grant usage on schema public to authenticated;
-grant select, update on public.profiles to authenticated;
-grant select, insert, update on public.settings to authenticated;
-grant select, insert, update, delete on public.reminders to authenticated;
